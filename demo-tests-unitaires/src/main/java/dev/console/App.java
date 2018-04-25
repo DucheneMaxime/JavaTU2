e0e8989b169cad24d9902731db03124e55b68ce1
Maxime Duchene <duchene.maxime@outlook.fr>
2018-04-25 12:29:21 +0200

    Exercices AssertJ et System-Rules terminés

---
+++ demo-tests-unitaires/src/main/java/dev/console/App.java
@@ -0,0 +1,32 @@
+package dev.console;
+
+import java.util.Scanner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import dev.service.CalculService;
+import dev.service.CalculServiceTest;
+
+public class App {
+	private Scanner scanner;
+	private CalculService calculatrice;
+
+	private static final Logger LOG = LoggerFactory.getLogger(CalculServiceTest.class);
+
+	public App(Scanner scanner, CalculService calculatrice) {
+		this.scanner = scanner;
+		this.calculatrice = calculatrice;
+	}
+
+	protected void afficherTitre() {
+		LOG.info("**** Application Calculatrice ****");
+	}
+
+	public void demarrer() {
+		afficherTitre();
+	}
+
+	protected void evaluer(String expression) {
+	}
+}
